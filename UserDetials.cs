6fc2d346824715b4c630591df56841c7f064cae3
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-21 15:09:50 +0530

    Added Regex Function for LastName Validation

--- UserDetials.cs
+++ UserDetials.cs
@@ -10,10 +10,16 @@ namespace UserRegistrationProblem
     public class UserDetials
     {
         public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        //LastNameValidation
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         public bool validateFirstname(string Firstname)
         {
             return Regex.IsMatch(Firstname, REGEX_FIRSTNAME);
 
+        }
+        public bool validateLastname(string Lastname)
+        {
+            return Regex.IsMatch(Lastname, REGEX_LASTNAME);
         }
     }
 }
