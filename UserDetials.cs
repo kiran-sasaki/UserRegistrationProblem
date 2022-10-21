30b6fc2ad660505f7b5577a5278ae49236a06ff4
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-21 15:21:16 +0530

    Added Regex Function for Email

--- UserDetials.cs
+++ UserDetials.cs
@@ -12,6 +12,7 @@ namespace UserRegistrationProblem
         public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         //LastNameValidation
         public static string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string REGEX_EMAILADDRESS = "^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$";
         public bool validateFirstname(string Firstname)
         {
             return Regex.IsMatch(Firstname, REGEX_FIRSTNAME);
@@ -20,6 +21,10 @@ namespace UserRegistrationProblem
         public bool validateLastname(string Lastname)
         {
             return Regex.IsMatch(Lastname, REGEX_LASTNAME);
+        }
+        public bool validateEmailAddress(string EmailAddress)
+        {
+            return Regex.IsMatch(EmailAddress, REGEX_EMAILADDRESS);
         }
     }
 }
