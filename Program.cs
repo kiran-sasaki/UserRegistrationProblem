cf9d13862648e7a5cf7bedb726b1e91d8da74154
kiran-sasaki <108934828+kiran-sasaki@users.noreply.github.com>
2022-10-21 15:30:48 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,22 +7,22 @@
             //Console.WriteLine("Welcome To User Registeration Problem");
             UserDetials regexProb = new UserDetials();
 
-            //Console.WriteLine("please enter Firstname");
-            //string FirstName = Console.ReadLine();
+            Console.WriteLine("please enter Firstname");
+            string FirstName = Console.ReadLine();
 
-            //bool Fname = regexProb.validateFirstname(FirstName);
-            //if (Fname)
-            //    Console.WriteLine("firstname Matched");
-            //else
-            //    Console.WriteLine("FirstName not matched");
-            ////UC2
-            //Console.WriteLine("please enter LastName");
-            //string LastName = Console.ReadLine();
-            //bool Lname = regexProb.validateLastname(LastName);
-            //if (Lname)
-            //    Console.WriteLine("Lastname Matched");
-            //else
-            //    Console.WriteLine("LastName not matched");
+            bool Fname = regexProb.validateFirstname(FirstName);
+            if (Fname)
+                Console.WriteLine("firstname Matched");
+            else
+                Console.WriteLine("FirstName not matched");
+            //UC2
+            Console.WriteLine("please enter LastName");
+            string LastName = Console.ReadLine();
+            bool Lname = regexProb.validateLastname(LastName);
+            if (Lname)
+                Console.WriteLine("Lastname Matched");
+            else
+                Console.WriteLine("LastName not matched");
             //UC3
             Console.WriteLine("please enter The EmailAdress: ");
             string Emailaddress = Console.ReadLine();
