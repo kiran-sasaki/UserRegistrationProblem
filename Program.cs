54c8c01d687fcece900155ed7ba743f8d87010f1
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-21 15:52:17 +0530

    Added Regex Function for PhoneNumber

--- Program.cs
+++ Program.cs
@@ -7,22 +7,22 @@
             //Console.WriteLine("Welcome To User Registeration Problem");
             UserDetials regexProb = new UserDetials();
 
-            //Console.WriteLine("please enter Firstname");
-            //string FirstName = Console.ReadLine();
+            Console.WriteLine("please enter Firstname");
+            string FirstName = Console.ReadLine();
 
-            //bool Fname = regexProb.validateFirstname(FirstName);
-            //if (Fname)
-            //    Console.WriteLine("firstname Matched");
-            //else
-            //    Console.WriteLine("FirstName not matched");
-            ////UC2
-            //Console.WriteLine("please enter LastName");
-            //string LastName = Console.ReadLine();
-            //bool Lname = regexProb.validateLastname(LastName);
-            //if (Lname)
-            //    Console.WriteLine("Lastname Matched");
-            //else
-            //    Console.WriteLine("LastName not matched");
+            bool Fname = regexProb.validateFirstname(FirstName);
+            if (Fname)
+                Console.WriteLine("firstname Matched");
+            else
+                Console.WriteLine("FirstName not matched");
+            //UC2
+            Console.WriteLine("please enter LastName");
+            string LastName = Console.ReadLine();
+            bool Lname = regexProb.validateLastname(LastName);
+            if (Lname)
+                Console.WriteLine("Lastname Matched");
+            else
+                Console.WriteLine("LastName not matched");
             //UC3
             Console.WriteLine("please enter The EmailAdress: ");
             string Emailaddress = Console.ReadLine();
@@ -32,6 +32,14 @@
                 Console.WriteLine("Email Matched");
             else
                 Console.WriteLine("Email not matched");
+            //UC4
+            Console.WriteLine("please enter PhoneNumber");
+            string Phonenumber = Console.ReadLine();
+            bool PhnNO = regexProb.validatephonenumber(Phonenumber);
+            if (PhnNO)
+                Console.WriteLine("Phn NO Matched");
+            else
+                Console.WriteLine("Phn No not matched");
         }
     }
 }
