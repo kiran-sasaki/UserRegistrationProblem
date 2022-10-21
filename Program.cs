52cad435e4e24098f8a7e7a833651b273223da84
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-21 15:00:54 +0530

    Added Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registeration Problem");
+        }
+    }
+}
